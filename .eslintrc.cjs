2fb2225c59584bda69174e130ffa4fae04d96e13
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2023-11-10 13:33:17 -0800

    Set up GitHub pages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,5 +18,6 @@ module.exports = {
     ],
     "no-unused-vars": "off",
     "react/no-unknown-property": "off",
+    "no-undef": "off",
   },
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Checked if it's in production mode, if it is, set the base path to /react-todo-list/, if not, set it to /.
+  base: process.env.NODE_ENV === "production" ? "/react-todo-list" : "/",
   plugins: [react()],
 })
