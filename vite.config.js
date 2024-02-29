09b138f8d806800e0a937b2a436910e52520206e
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-02-29 03:17:56 -0800

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   // Checked if it's in production mode, if it is, set the base path to /react-todo-list/, if not, set it to /.
-  base: process.env.NODE_ENV === "production" ? "/react-todo-list/" : "/",
+  // base: process.env.NODE_ENV === "production" ? "/react-todo-list/" : "/",
   plugins: [react()],
 })
